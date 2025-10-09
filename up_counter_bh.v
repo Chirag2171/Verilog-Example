b205259a1e931db95e8f2445ebf551c343b29ec4
Chirag2171 <chiragrao2171@gmail.com>
2025-10-09 23:30:27 +0530

    Add files via upload

--- up_counter_bh.v
+++ up_counter_bh.v
@@ -1,32 +1,14 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/08/2025 12:10:58 AM
-// Design Name: 
-// Module Name: up_counter_bh
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module up_counter_bh(rst, clk, out);
-input rst, clk;
-output reg [3:0] out;
- always @(posedge clk or posedge rst)begin
- if(rst)
- out <= 4'd0;
- else
- out <= out + 4'd1;
- end
+module counter (clk,rst,updwn,count);
+input clk,rst,updwn;
+output reg [3:0] count;
+always @(posedge clk or posedge rst)
+begin
+if(rst)
+count <= 4'b0000;
+else 
+count <= (updwn) ? (count + 4'b1) : (count - 4'd1);
+end
 endmodule
